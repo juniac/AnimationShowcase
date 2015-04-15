fee3a467db654cf22baeb65620dd459e6f549911
Jeong-un, Kim <juniac79@gmail.com>
2015-04-15 19:13:42 +0900

    Add sample buttons

--- AnimationShowcase/ViewController.swift
+++ AnimationShowcase/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dampingValueLabel: UILabel!
+    @IBOutlet weak var velocityValueLabel: UILabel!
+    @IBOutlet weak var durationValueLabel: UILabel!
+    @IBOutlet weak var dampingSlider: UISlider!
+    @IBOutlet weak var velocitySlider: UISlider!
+    @IBOutlet weak var durationSlider: UISlider!
+    
+    @IBOutlet weak var animationOptionSegment: UISegmentedControl!
+    
+    var sampleView:UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sampleView = UIView(frame: CGRectMake(0, 100, 50, 50))
+        sampleView.backgroundColor = UIColor.redColor()
+        self.view.addSubview(sampleView)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dampingSliderAction(sender: UISlider) {
+        self.dampingValueLabel.text = NSString(format: "%.2f", sender.value) as String
+    }
 
+    @IBAction func velocitySliderAction(sender: UISlider) {
+        velocityValueLabel.text = NSString(format: "%.2f", sender.value) as String
+    }
+    @IBAction func durationSliderAction(sender: UISlider) {
+        durationValueLabel.text = NSString(format: "%.2f", sender.value) as String
+    }
+    @IBAction func resetButtonAction(sender: UIButton) {
+        sampleView.transform = CGAffineTransformIdentity
+    }
+    @IBAction func startButtonAction(sender: AnyObject) {
+        var option:UIViewAnimationOptions!
+        switch animationOptionSegment.selectedSegmentIndex {
+        case 0:
+            option = UIViewAnimationOptions.CurveEaseIn
+            break
+        case 1:
+            option = UIViewAnimationOptions.CurveEaseOut
+            break
+        case 2:
+            option = UIViewAnimationOptions.CurveEaseInOut
+            break
+        case 3:
+            option = UIViewAnimationOptions.CurveLinear
+            break
+        default:
+            option = UIViewAnimationOptions.CurveEaseInOut
+            break
+            
+        }
+
+        
+        UIView.animateWithDuration(NSTimeInterval(durationSlider.value),
+            delay: 0,
+            usingSpringWithDamping:CGFloat(dampingSlider.value),
+            initialSpringVelocity: CGFloat(velocitySlider.value),
+            options: option, animations: {
+                self.sampleView.transform = CGAffineTransformMakeTranslation(100, 0)
+                
+            }, completion: { finished in
+        })
+    }
 }
 

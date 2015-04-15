fc14f786c6ad77c17ff0aca50d0d19abffe231b2
Jeong-un, Kim <juniac79@gmail.com>
2015-04-16 01:14:47 +0900

    Fix Animation Option default Value

--- AnimationShowcase/ViewController.swift
+++ AnimationShowcase/ViewController.swift
@@ -13,25 +13,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var dampingValueLabel: UILabel!
     @IBOutlet weak var velocityValueLabel: UILabel!
     @IBOutlet weak var durationValueLabel: UILabel!
+    @IBOutlet weak var delayValueLabel: UILabel!
     @IBOutlet weak var dampingSlider: UISlider!
     @IBOutlet weak var velocitySlider: UISlider!
     @IBOutlet weak var durationSlider: UISlider!
+    @IBOutlet weak var delaySlider: UISlider!
     
     @IBOutlet weak var animationOptionSegment: UISegmentedControl!
     
-    var sampleView:UIView!
+
+    var sampleViews = [UIView]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        sampleView = UIView(frame: CGRectMake(0, 100, 50, 50))
-        sampleView.backgroundColor = UIColor.redColor()
-        self.view.addSubview(sampleView)
+
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func addButtonAction(sender: UIButton) {
+        let y = sampleViews.count * 50 + 40
+        let frame = CGRectMake(10, CGFloat(y), 40, 40)
+        var sampleView = UIView(frame:frame)
+        sampleView.backgroundColor = UIColor.redColor()
+        self.view.addSubview(sampleView)
+        sampleViews.append(sampleView)
+        
+    }
+    @IBAction func removeButtonAction(sender: UIButton) {
+        
     }
 
     @IBAction func dampingSliderAction(sender: UISlider) {
@@ -44,9 +57,16 @@ class ViewController: UIViewController {
     @IBAction func durationSliderAction(sender: UISlider) {
         durationValueLabel.text = NSString(format: "%.2f", sender.value) as String
     }
-    @IBAction func resetButtonAction(sender: UIButton) {
-        sampleView.transform = CGAffineTransformIdentity
+    @IBAction func delaySliderAction(sender: UISlider) {
+        delayValueLabel.text = NSString(format: "%.2f", sender.value) as String
     }
+    @IBAction func resetButtonAction(sender: UIButton) {
+        for sampleView in sampleViews {
+            sampleView.transform = CGAffineTransformIdentity
+        }
+        
+    }
+
     @IBAction func startButtonAction(sender: AnyObject) {
         var option:UIViewAnimationOptions!
         switch animationOptionSegment.selectedSegmentIndex {
@@ -68,16 +88,23 @@ class ViewController: UIViewController {
             
         }
 
+
+        for (index,sampleView) in enumerate(sampleViews) {
+            var delay = Double(delaySlider.value) * Double(index)
+            println(delay)
+            UIView.animateWithDuration(NSTimeInterval(durationSlider.value),
+                delay: NSTimeInterval(delay),
+                usingSpringWithDamping:CGFloat(dampingSlider.value),
+                initialSpringVelocity: CGFloat(velocitySlider.value),
+                options: option, animations: {
+                    sampleView.transform = CGAffineTransformMakeTranslation(100, 0)
+                    
+                }, completion: { finished in
+            })
+
+        }
         
-        UIView.animateWithDuration(NSTimeInterval(durationSlider.value),
-            delay: 0,
-            usingSpringWithDamping:CGFloat(dampingSlider.value),
-            initialSpringVelocity: CGFloat(velocitySlider.value),
-            options: option, animations: {
-                self.sampleView.transform = CGAffineTransformMakeTranslation(100, 0)
-                
-            }, completion: { finished in
-        })
+        
     }
 }
 
